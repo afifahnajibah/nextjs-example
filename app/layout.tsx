f04993834c4c1d7c0296b993d7934668882e7cc2
afifah arif <afifahnajibaharif@gmail.com>
2024-01-04 23:32:41 +0800

    revert to initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { inter } from '@/app/fonts';
+import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import AcmeLogo from '@/app/acme-logo';
+import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-import '@/app/global.css'
+import '@/app/ui/global.css'
 import Image from 'next/image';
 
 export default function Page() {
