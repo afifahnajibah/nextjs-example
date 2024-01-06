887bd4b60022fce01852037361d0f43f97cb3607
afifah arif <afifahnajibaharif@gmail.com>
2024-01-07 03:04:56 +0800

    remove comment

--- app/dashboard/gallery/page.tsx
+++ app/dashboard/gallery/page.tsx
@@ -3,7 +3,7 @@
 import '@/app/ui/global.css'
 import '@/app/ui/gallery.css'
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React from 'react';
 import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai"
 
 import Slider from "react-slick";
@@ -12,17 +12,6 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 export default function Gallery () {
-    
-    
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const nextSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % cards.length);
-    };
-
-    const prevSlide = () => {
-        setCurrentIndex((prevIndex) => (prevIndex - 1 + cards.length) % cards.length);
-    };
 
     const cards = [
         {
@@ -57,8 +46,6 @@ export default function Gallery () {
         },
     ]
 
-    // const duplicatedCards = [...cards, ...cards]; //to make the images loop seamlessly, after image 1 straight display image 5
-
     const nextArrow = (
         <button className="absolute top-1/2 left-4 transform -translate-y-1/2" >
             <AiOutlineArrowRight/>
@@ -67,7 +54,7 @@ export default function Gallery () {
 
     const prevArrow = (
         <button className="absolute top-1/2 right-4 transform -translate-y-1/2">
-            <AiOutlineArrowRight/>
+            <AiOutlineArrowLeft/>
         </button>
     )
 
@@ -104,99 +91,10 @@ export default function Gallery () {
                         </div>
                     </div>
                 </div>
-
             </>
             ))} 
         </Slider>
     </div>
-    
-
-    {/* <div className="relative overflow-hidden bg-green-500">
-        
-        <div className="flex justify-center p-5 m-5 bg-red-500 card-container">
-            {duplicatedCards.slice(currentIndex, currentIndex + 3).map((card, index) => (
-                <div key={index} className="flex-shrink-0 relative w-1/3 card">
-
-                        <div className={`${index === 1 ? 'scale-125' : 'scale-75 transition-opacity opacity-70'} p-5 bg-slate-800 rounded-md text-white font-light`}> 
-                            <div className='flex flex-col'>
-                                <p>{card.description}</p>
-                            </div>
-                            <div className='flex flex-row border-t-slate-500 border-t-2 justify-evenly pt-3'>
-                                <Image src={card.image} alt="" width={60} height={60} className='rounded-full'/>
-                                <div className='flex flex-col'>
-                                    <h2 className='uppercase font-bold text-amber-500'>{card.title}</h2>
-                                    <h4>{card.position}</h4>
-                                </div>
-                            </div>
-                        </div>
-
-                </div>
-            ))}
-        </div>
-
-        <button className="absolute top-1/2 left-4 transform -translate-y-1/2" onClick={prevSlide}>
-            <AiOutlineArrowLeft/>
-        </button>
-        <button className="absolute top-1/2 right-4 transform -translate-y-1/2" onClick={nextSlide}>
-            <AiOutlineArrowRight/>
-        </button>
-    </div> */}
     </>
     );
 };
-
-
-// export default function Gallery() {
-//     const duplicatedImages = [...images, ...images]; //to make the images loop seamlessly, after image 1 straight display image 5
-//     const [currentIndex, setCurrentIndex] = useState(0);
-
-//     const nextSlide = () => {
-//         setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-//     };
-
-//     const prevSlide = () => {
-//         setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-//     };
-
-//     return (
-//     <div className="relative bg-slate-300 overflow-hidden rounded-lg p-5">
-        
-//         <div 
-//             className="flex justify-center transition-transform ease-in-out duration-300 transform" 
-//             // style={{ width: `${images.length * 10}%`, translate: `-${(currentIndex / images.length) * 10}%` }}
-//         >
-//             {duplicatedImages.slice(currentIndex, currentIndex + 3).map((image, index) => (
-//                 <div key={index} className="flex-shrink-0 w-1/3">
-//                     <div className="relative" style={{ marginLeft: index === 1 ? 'auto' : '', marginRight: index === 1 ? 'auto' : '' }}>
-//                     <Image
-//                         src={image}
-//                         alt={`Slide ${index + 1}`}
-//                         width={100}
-//                         height={100}
-//                         layout="responsive"
-//                         className={`${index === 1 ? 'scale-110' : 'scale-50'} rounded-lg`}
-//                     />
-//                     {cards.map((card) =>
-//                     <>
-//                         <div className='border-black border-4'>
-//                             <h2>{card.title}</h2>
-//                             <p>{card.description}</p>
-//                         </div>
-//                     </>
-//                     )}
-                    
-//                     </div>
-//                 </div>
-//             ))}
-//         </div>
-
-//         <button className="absolute top-1/2 left-4 transform -translate-y-1/2" onClick={prevSlide}>
-//             <AiOutlineArrowLeft/>
-//         </button>
-//         <button className="absolute top-1/2 right-4 transform -translate-y-1/2" onClick={nextSlide}>
-//             <AiOutlineArrowRight/>
-//         </button>
-
-//     </div>
-//     );
-// };
