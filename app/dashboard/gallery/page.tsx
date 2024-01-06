f31ad69178cc11e99ca2a87101fb62f0775e3154
afifah arif <afifahnajibaharif@gmail.com>
2024-01-07 03:02:38 +0800

    adjust gallery code

--- app/dashboard/gallery/page.tsx
+++ app/dashboard/gallery/page.tsx
@@ -1,45 +1,19 @@
 "use client"; // for nextjs 13.4 user
 
 import '@/app/ui/global.css'
+import '@/app/ui/gallery.css'
 import Image from 'next/image';
 import React, { useState } from 'react';
-import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/ai"
 
-const cards = [
-    {
-        title: "card 1",
-        image: "/customers/amy-burns.png",
-        position:"CEO",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-    {
-        title: "card 2",
-        image: "/customers/balazs-orban.png",
-        position: "COO",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-    {
-        title: "card 3",
-        image: "/customers/evil-rabbit.png",
-        position: "CPU",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-    {
-        title: "card 4",
-        image:"/customers/lee-robinson.png",
-        position: "CPR",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-    {
-        title: "card 5",
-        image: "/customers/hector-simpson.png",
-        position: "CP",
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-    },
-]
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function Gallery() {
-    const duplicatedCards = [...cards, ...cards]; //to make the images loop seamlessly, after image 1 straight display image 5
+
+export default function Gallery () {
+    
+    
     const [currentIndex, setCurrentIndex] = useState(0);
 
     const nextSlide = () => {
@@ -50,15 +24,100 @@ export default function Gallery() {
         setCurrentIndex((prevIndex) => (prevIndex - 1 + cards.length) % cards.length);
     };
 
+    const cards = [
+        {
+            title: "card 1",
+            image: "/customers/amy-burns.png",
+            position:"CEO",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            title: "card 2",
+            image: "/customers/balazs-orban.png",
+            position: "COO",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            title: "card 3",
+            image: "/customers/evil-rabbit.png",
+            position: "CPU",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            title: "card 4",
+            image:"/customers/lee-robinson.png",
+            position: "CPR",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+        {
+            title: "card 5",
+            image: "/customers/hector-simpson.png",
+            position: "CP",
+            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+        },
+    ]
 
+    // const duplicatedCards = [...cards, ...cards]; //to make the images loop seamlessly, after image 1 straight display image 5
+
+    const nextArrow = (
+        <button className="absolute top-1/2 left-4 transform -translate-y-1/2" >
+            <AiOutlineArrowRight/>
+        </button>
+    )
+
+    const prevArrow = (
+        <button className="absolute top-1/2 right-4 transform -translate-y-1/2">
+            <AiOutlineArrowRight/>
+        </button>
+    )
+
+    const settings = {
+        className: "center",
+        centerMode: true,
+        infinite: true,
+        centerPadding: "0",
+        slidesToShow: 4,
+        speed: 1000, 
+        nextArrow: nextArrow,
+        prevArrow: prevArrow,
+    };
+
+    
 
     return (
-    <div className="relative overflow-hidden rounded-lg p-5">
-        <div className="flex justify-center">
+    <>
+    <div className='bg-gray-300 p-10'>
+        <Slider {...settings}>
+            {cards.map((card) => (
+            <>
+                <div className="p-5 bg-slate-800 rounded-md text-white font-light"> 
+                    <div className='flex flex-col'>
+                        <p className='mb-4 border-b-slate-500 border-b-2 pb-3'>{card.description}</p>
+                        <div className='flex flex-row items-center justify-between '>
+                            <div className='flex flex-row items-center'>
+                                <Image src={card.image} alt="" width={40} height={40} className='rounded-full'/>
+                                <div className='flex flex-col ml-3'>
+                                    <h2 className='uppercase font-bold text-amber-500'>{card.title}</h2>
+                                    <h4>{card.position}</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </>
+            ))} 
+        </Slider>
+    </div>
+    
+
+    {/* <div className="relative overflow-hidden bg-green-500">
+        
+        <div className="flex justify-center p-5 m-5 bg-red-500 card-container">
             {duplicatedCards.slice(currentIndex, currentIndex + 3).map((card, index) => (
-                <div key={index} className="flex-shrink-0 w-1/3 duration-700 ease-in-out">
-                    <div className="relative" style={{ marginLeft: index === 1 ? 'auto' : '', marginRight: index === 1 ? 'auto' : '' }}>
-                        <div className={`${index === 1 ? 'scale-125' : 'scale-75'} p-5 bg-slate-800 rounded-md text-white font-light relative`}> 
+                <div key={index} className="flex-shrink-0 relative w-1/3 card">
+
+                        <div className={`${index === 1 ? 'scale-125' : 'scale-75 transition-opacity opacity-70'} p-5 bg-slate-800 rounded-md text-white font-light`}> 
                             <div className='flex flex-col'>
                                 <p>{card.description}</p>
                             </div>
@@ -69,19 +128,8 @@ export default function Gallery() {
                                     <h4>{card.position}</h4>
                                 </div>
                             </div>
-
-                            {/* <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                                <span className="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl dark:text-gray-800">First Slide</span>
-                                <Image 
-                                    src="/gallery-2.png" 
-                                    className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" 
-                                    alt="..."
-                                    width={100}
-                                    height={100}
-                                />
-                            </div> */}
                         </div>
-                    </div>
+
                 </div>
             ))}
         </div>
@@ -92,7 +140,8 @@ export default function Gallery() {
         <button className="absolute top-1/2 right-4 transform -translate-y-1/2" onClick={nextSlide}>
             <AiOutlineArrowRight/>
         </button>
-    </div>
+    </div> */}
+    </>
     );
 };
 
